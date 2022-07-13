cc71b2217bb085994d9ccd40d37d7cee88690660
Jiří Vrba <jiri@vrba.dev>
2022-07-13 10:47:46 +0200

    Add work in progress account repository

---
+++ src/main/kotlin/cz/bettervse/api/repository/AccountRepository.kt
@@ -0,0 +1,13 @@
+package cz.bettervse.api.repository
+
+import cz.bettervse.api.domain.Account
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+
+@Repository
+interface AccountRepository : ReactiveCrudRepository<Account, Int> {
+
+    fun findAccountByUsernameAndCode(username: String, code: String): Mono<Account>
+
+}
