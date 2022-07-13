99de789d7909da4b20d4c4083fabc254081296b2
Jiří Vrba <jiri@vrba.dev>
2022-07-13 10:38:20 +0200

    Add database configuration

--- src/main/kotlin/cz/bettervse/api/BetterVseApiApplication.kt
+++ src/main/kotlin/cz/bettervse/api/BetterVseApiApplication.kt
@@ -1,10 +1,27 @@
 package cz.bettervse.api
 
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class BetterVseApiApplication
+class BetterVseApiApplication {
+    @Bean
+    fun connectionFactory(datasource: DataSourceProperties): ConnectionFactory {
+        val url = datasource.url.replaceFirst("jdbc:", "r2dbc:")
+        val options = ConnectionFactoryOptions.builder()
+            .from(ConnectionFactoryOptions.parse(url))
+            .option(ConnectionFactoryOptions.USER, datasource.username)
+            .option(ConnectionFactoryOptions.PASSWORD, datasource.password)
+            .build()
+
+        return ConnectionFactories.get(options)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<BetterVseApiApplication>(*args)
