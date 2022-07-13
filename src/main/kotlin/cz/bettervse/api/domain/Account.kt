bf689073639f9b1df9ad9293eec8d735c6cd5b33
Jiří Vrba <jiri@vrba.dev>
2022-07-13 10:45:58 +0200

    Add account domain data class

---
+++ src/main/kotlin/cz/bettervse/api/domain/Account.kt
@@ -0,0 +1,17 @@
+package cz.bettervse.api.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("accounts")
+data class Account(
+    @Id
+    val id: Int = 0,
+
+    @Column("username")
+    val username: String,
+
+    @Column("verifiation_code")
+    val code: String? = null
+)
